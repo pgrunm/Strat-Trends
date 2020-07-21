24da68f6091a0ad63f398837857e9e3c180dede6
Warryz <8317771+Warryz@users.noreply.github.com>
2020-07-21 22:16:46 +0200

    Move test2 into test.py.

--- test.py
+++ test.py
@@ -1,7 +1,29 @@
-import newspaper as n
-import feedparser
+import asyncio
 import json
 import logging
+import time
+
+import aiohttp
+import feedparser
+import newspaper as n
+
+
+async def download_site(url):
+    # async with session.get(url) as response:
+    #     print("Read {0} from {1}".format(response.content_length, url))
+    return feedparser.parse(url)
+
+
+async def download_all_sites(sites):
+    tasks = []
+    for url in sites:
+        task = asyncio.ensure_future(download_site(url))
+        tasks.append(task)
+    liste = await asyncio.gather(*tasks, return_exceptions=True)
+    # print(liste)
+
+    return liste
+
 
 if __name__ == "__main__":
 
@@ -37,3 +59,15 @@ if __name__ == "__main__":
         '''
     else:
         logging.info(f'HTTP Status ist {d["status"]}')
+
+    # Async stuff
+    sites = [
+        "https://rss.golem.de/rss.php?feed=ATOM1.0",
+        "https://www.tagesschau.de/xml/rss2_https/",
+    ]
+    start_time = time.time()
+
+    # Retrieve all elements from the listed feeds
+    entries = asyncio.get_event_loop().run_until_complete(download_all_sites(sites))
+    duration = time.time() - start_time
+    print(f"Downloaded {len(sites)} sites in {duration} seconds")
