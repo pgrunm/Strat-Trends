063dd1c91a84aad0c409aa3e88b58dcd0e3be13c
Warryz <8317771+Warryz@users.noreply.github.com>
2020-07-26 22:45:43 +0200

    Updated the test script.

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ import sqlite3
 import aiohttp
 import feedparser
 import newspaper as n
+from ratelimit import limits, sleep_and_retry
+from urllib.parse import urlparse
 
 
 async def download_site(url):
@@ -22,9 +24,45 @@ async def download_all_sites(sites):
         task = asyncio.ensure_future(download_site(url))
         tasks.append(task)
     liste = await asyncio.gather(*tasks, return_exceptions=True)
-    # print(liste)
-
     return liste
+
+
+@sleep_and_retry
+@limits(calls=5, period=1)
+async def download_article(entry, source_id):
+    article = Article(entry['link'], fetch_images=False, language='de')
+    article.download()
+    article.parse()
+
+    # Parse the keywords
+    # article.nlp()
+
+    article.text.replace("'", "")
+
+    # We need the entry, the content, the title and keywords.
+    return (article.text, source_id, entry['link'], entry['title'])
+
+
+async def download_articles(article_list, postmortem_titles, feed_url_dict):
+    # List to return
+    tasks = []
+
+    # Iterateover all the feeds
+    for feed in article_list:
+        # Access the entries within each feed
+        for entry in feed['entries']:
+            # Prepare the foreign key of the source id
+            source_id = feed_url_dict[feed.href]
+            # Check if the title is within our database, if not download and save the postmortem.
+            if entry['title'] not in postmortem_titles:
+                logging.debug(
+                    f"Article {entry['title']} not in database, downloading it from {entry['link']}")
+                # Download the article
+                tasks.append(asyncio.ensure_future(
+                    download_article(entry, source_id)))
+
+            # We need the url, the content, the title and keywords. But watch out, keywords may be an empty list!
+    return await asyncio.gather(*tasks, return_exceptions=True)
 
 
 if __name__ == "__main__":
@@ -35,8 +73,8 @@ if __name__ == "__main__":
 
     # Logging Handler
     # Configure logging
-    logging.basicConfig(format='[%(levelname)s]: %(asctime)s %(message)s', datefmt='%d.%m.%Y %H:%M:%S',
-                        level=logging.DEBUG, filemode='w', filename='app.log')
+    logging.basicConfig(handlers=[logging.FileHandler(filename='app.log', mode='w', encoding='utf-8')],
+                        level=logging.DEBUG, format='[%(levelname)s]: %(asctime)s %(message)s', datefmt='%d.%m.%Y %H:%M:%S')
 
     example_url = 'http://127.0.0.1:8020/blog'
     d = feedparser.parse(example_url)
@@ -80,7 +118,7 @@ if __name__ == "__main__":
 
     c.execute('select title from Postmortems;')
 
-    # Fetch all rows
+    # Fetch all postmortem title rows
     try:
         postmortem_titles = c.fetchall()
         logging.debug(f'Query returned: {postmortem_titles}')
@@ -88,24 +126,40 @@ if __name__ == "__main__":
         logging.critical(
             f'Error occured while retrieving all rows: {sqlite_error}')
 
-    article_list = list()
+    # Need a dictionary to determine what site has which id
+    feed_url_dict = dict()
+    c.execute('select source_id, url from Sources;')
+    try:
+        feed_urls = c.fetchall()
+        logging.debug(f'Query returned: {feed_urls}')
+    except sqlite3.Error as sqlite_error:
+        logging.critical(
+            f'Error occured while retrieving all rows: {sqlite_error}')
 
-    # Iterateover all the feeds
-    for feed in feeds:
-        # Access the entries within each feed
-        for entry in feed['entries']:
+    # Fill the dictionary
+    for source_id, url in feed_urls:
+        logging.debug(f'Adding the url {url} as key for source id {source_id}')
+        feed_url_dict[url] = source_id
 
-            # Check if the title is within our database, if not download and save the postmortem.
-            if entry['title'] not in postmortem_titles:
-                logging.debug(
-                    f"Article {entry['title']} not in database, downloading it from {entry['link']}")
-                # Download the article
-                article = Article(entry['link'])
-                article.download()
-                article.parse()
+    articles = asyncio.get_event_loop().run_until_complete(
+        download_articles(feeds, postmortem_titles, feed_url_dict))
+    print(
+        f"Downloaded {len(articles)} articles in {time.time() - start_time} seconds")
 
-                # We need the url, the content, the title and keywords. But watch out, keywords may be an empty list!
-                article_list.append((entry['link'], entry['title'],
-                                     article.text, article.keywords))
+    # Save the data to the database
+    try:
+        c.executemany(
+            'INSERT INTO Postmortems (content, fk_source_id, url, title) VALUES (?,?,?,?);', articles)
+        conn.commit()
+    except sqlite3.Error as sqlite_error:
+        logging.critical(
+            f'Error occured while inserting rows: {sqlite_error}')
 
-    print(article_list)
+    print(
+        f"Finished in {time.time() - start_time} seconds")
+    conn.close()
+
+    # content, fk_source_id, pm_id, url, title
+    for content, fk_source_id, url, title in articles:
+        logging.info(
+            f"INSERT INTO Postmortems (content, fk_source_id, url, title) VALUES ('{content}', {fk_source_id}, '{url}', '{title}');")
