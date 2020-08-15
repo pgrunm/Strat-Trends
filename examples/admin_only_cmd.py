1de97dec3be5ca0fca55d335b189081bc0d31a21
Warryz <8317771+Warryz@users.noreply.github.com>
2020-08-15 13:24:17 +0200

    Added an admin only command.

---
+++ examples/admin_only_cmd.py
@@ -0,0 +1,7 @@
+from errbot import BotPlugin, arg_botcmd, botcmd, logging, webhook
+
+
+@botcmd
+def example_command(self, msg, args, admin_only=True):
+    # Just a simple example to demonstrated how admin only commands work.
+    self.warn_admins('Warning, something really bad happened!')
