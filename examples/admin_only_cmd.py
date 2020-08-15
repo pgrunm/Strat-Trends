ff5680530c4667a555435dd54ce9edf22e464dc6
Warryz <8317771+Warryz@users.noreply.github.com>
2020-08-15 16:59:39 +0200

    Fixed line length.

--- examples/admin_only_cmd.py
+++ examples/admin_only_cmd.py
@@ -3,5 +3,5 @@ from errbot import BotPlugin, arg_botcmd, botcmd, logging, webhook
 
 @botcmd
 def example_command(self, msg, args, admin_only=True):
-    # Just a simple example to demonstrated how admin only commands work.
+    # Just a simple example to show how admin only commands work.
     self.warn_admins('Warning, something really bad happened!')
